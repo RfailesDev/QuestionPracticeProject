9b2706d71579791b785e777e1eabb332ee2876be
RfailesDev <86327915+RfailesDev@users.noreply.github.com>
2023-10-13 13:20:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// Основной JavaScript для обычного режима
+$(document).ready(function () {
+    $('#gotoQuestions').click(function () {
+        window.location.href = "questions.html";
+    });
+
+    $('#adminButton').click(function () {
+        // Реализуйте переход к режиму администратора
+    });
+});
+
+$(document).ready(function() {
+    $('.scrolling-frame').on('click', '.showMoreButton', function() {
+        var container = $(this).closest('.question-container');
+        var button = container.find('.showMoreButton');
+        var questionPart2 = container.find('.question-part2');
+        var answerContainer = container.find('.answer-container');
+        var txtColumn = container.find('.txt-column');
+
+        if (questionPart2.hasClass('expanded')) {
+            questionPart2.removeClass('expanded');
+            if (button.hasClass('hide-answer-cls')) {
+                button.removeClass('hide-answer-cls');
+            }
+            button.text('Показать ответ');
+            questionPart2.css('max-height', '0');
+            questionPart2.css('opacity', '0'); // Скрываем контент при скрытии
+
+            txtColumn.css('opacity', '0');
+
+            answerContainer.removeClass('showed');
+        } else {
+            questionPart2.addClass('expanded');
+            button.text('Скрыть ответ');
+            button.addClass('hide-answer-cls');
+            questionPart2.css('max-height', answerContainer.height() + 30 + 'px'); // Установка максимальной высоты
+            questionPart2.css('opacity', '1'); // Сделаем контент видимым при раскрытии
+
+            txtColumn.css('opacity', '1');
+
+
+            answerContainer.addClass('showed');
+        }
+    });
+});
